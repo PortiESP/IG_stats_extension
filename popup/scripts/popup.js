9863a964efbb81f54e56a30679d8daa950e146f9
Porti <elp0rti201@gmail.com>
2022-09-04 17:54:32 +0200

    almos finish, moved pages

--- popup/scripts/popup.js
+++ popup/scripts/popup.js
@@ -1,6 +1,6 @@
 
 function setToolListener(param){
-    chrome.tabs.create({url: `popup/pages/${param}`})
+    chrome.tabs.create({url: `pages/${param}`})
     .then( tab => {
         console.log(tab)
         chrome.tabs.sendMessage( tab.id, { message: USERDATA } )
@@ -14,6 +14,14 @@ function setToolListener(param){
 chrome.runtime.sendMessage({subject: "FETCH"}, res => {
     USERDATA = res
     setSummaryStatus(USERDATA)
+
+    document.querySelector(".div__summary a:nth-child(2)").href = "file://" + USERDATA.path + "followers_and_following/followers.html" 
+    document.querySelector(".div__summary a:nth-child(3)").href = "file://" + USERDATA.path + "followers_and_following/following.html" 
+    document.querySelector(".div__summary a:nth-child(4)").href = "file://" + USERDATA.path + "your_topics/your_reels_topics.html" 
+    document.querySelector(".div__summary a:nth-child(5)").href = "file://" + USERDATA.path + "information_about_you/ads_interests.html" 
+    document.querySelector(".div__summary a:nth-child(6)").href = "file://" + USERDATA.path + "login_and_account_creation/login_activity.html" 
+    document.querySelector(".div__summary a:nth-child(7)").href = "file://" + USERDATA.path + "device_information/devices.html" 
+    document.querySelector(".div__summary a:nth-child(8)").href = "file://" + USERDATA.path + "followers_and_following/pending_follow_requests.html" 
 })
 
 // Remove data btn
@@ -25,6 +33,10 @@ document.querySelector(".button__deleteData").onclick = () => {
     }
 }
 
-document.querySelectorAll(".div__stats_item")[0].onclick = () => setToolListener("datalist/datalist.html?notFollowingBack")
-document.querySelectorAll(".div__stats_item")[1].onclick = () => setToolListener("datalist/datalist.html?myInterests")
-document.querySelectorAll(".div__stats_item")[2].onclick = () => setToolListener("activity/activity.html?login")
+window.addEventListener("DOMContentLoaded", ()=>{
+    
+    document.querySelectorAll(".div__stats_item")[0].onclick = () => setToolListener("datalist/datalist.html?notFollowingBack")
+    document.querySelectorAll(".div__stats_item")[1].onclick = () => setToolListener("datalist/datalist.html?myInterests")
+    document.querySelectorAll(".div__stats_item")[2].onclick = () => setToolListener("activity/activity.html?login")
+
+})
